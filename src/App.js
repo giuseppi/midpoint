404981ae4e44b80e9b0c1089ef7d9196e5e2a059
giuseppi <giuseppipelayo@gmail.com>
2024-11-21 09:26:14 -0800

    Created Login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
 
 function App() {
+  const handleLogin = (username, password) => {
+    console.log('Logged in:', { username, password });
+    // Add additional login logic here
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login onLogin={handleLogin} />
     </div>
   );
 }
