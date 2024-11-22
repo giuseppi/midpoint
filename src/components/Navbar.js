d973debf2dee3d547515ee885634488b6befcaf1
giuseppi <giuseppipelayo@gmail.com>
2024-11-22 13:26:12 -0800

    updated NavBar to be functionable, added hover transitions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import { Navbar } from 'flowbite-react';
 import React from 'react';
-import '../index.css';
 import HomeButton from './HomeButton';
 
 const MidpointNavbar = () => {
@@ -8,21 +7,34 @@ const MidpointNavbar = () => {
     <Navbar
       fluid={true}
       rounded={true}
-      className="bg-gray-800 bg-opacity-80 backdrop-blur-sm text-white shadow-md"
+      className="bg-gray-800 bg-opacity-80 backdrop-blur-sm dark:text-white shadow-md"
     >
       <Navbar.Brand href="/">
         <div className="flex items-center space-x-3">
           <HomeButton />
-          <span className="whitespace-nowrap text-xl font-semibold text-white">Midpoint</span>
+          <span className="whitespace-nowrap text-xl font-semibold dark:text-white">Midpoint</span>
         </div>
       </Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse>
+      <Navbar.Toggle className="text-gray-400 hover:text-gray-400 hover:bg-gray-800 transition duration-300" />
+      <Navbar.Collapse className="lg:flex lg:space-x-4 space-y-2">
         <Navbar.Link
           href="/"
-          className="navbar-link"
+          active={true}
+          className="hover:text-gray-300 hover:bg-gray-700 font-bold px-3 py-2 rounded-md transition duration-300"
         >
           Home
+        </Navbar.Link>
+        <Navbar.Link
+          href="/about"
+          className="hover:text-gray-300 hover:bg-gray-700 font-bold px-3 py-2 rounded-md transition duration-300"
+        >
+          About
+        </Navbar.Link>
+        <Navbar.Link
+          href="/contact"
+          className="hover:text-gray-300 hover:bg-gray-700 font-bold px-3 py-2 rounded-md transition duration-300 mr-2"
+        >
+          Contact
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
