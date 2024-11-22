148bc955b0875f2bd34005e064521cbb550dd898
giuseppi <giuseppipelayo@gmail.com>
2024-11-22 13:26:36 -0800

    added active tab functionality on Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,12 @@
 import { Navbar } from 'flowbite-react';
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import HomeButton from './HomeButton';
 
 const MidpointNavbar = () => {
+  const location = useLocation();
+  const pathname = location.pathname;
+
   return (
     <Navbar
       fluid={true}
@@ -19,19 +23,21 @@ const MidpointNavbar = () => {
       <Navbar.Collapse className="lg:flex lg:space-x-4 space-y-2">
         <Navbar.Link
           href="/"
-          active={true}
+          active={pathname === '/'}
           className="hover:text-gray-300 hover:bg-gray-700 font-bold px-3 py-2 rounded-md transition duration-300"
         >
           Home
         </Navbar.Link>
         <Navbar.Link
           href="/about"
+          active={pathname === '/about'}
           className="hover:text-gray-300 hover:bg-gray-700 font-bold px-3 py-2 rounded-md transition duration-300"
         >
           About
         </Navbar.Link>
         <Navbar.Link
           href="/contact"
+          active={pathname === '/contact'}
           className="hover:text-gray-300 hover:bg-gray-700 font-bold px-3 py-2 rounded-md transition duration-300 mr-2"
         >
           Contact
