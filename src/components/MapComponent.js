8e11d3db223f116388c847e6ec8e2d086a88b875
giuseppi <giuseppipelayo@gmail.com>
2024-11-25 10:39:08 -0800

    Created initial map view and markers

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -9,21 +9,29 @@ const MapComponent = () => {
     console.error('Mapbox access token is missing.');
     return <div>Error: Mapbox access token is not defined.</div>;
   }
+
+  const initialView = {
+    longitude: -117.8265,
+    latitude: 33.6846,
+    zoom: 12,
+  };
+
+  const initialMarker = {
+    longitude: initialView.longitude + 0.01, // Shift slightly east
+    latitude: initialView.latitude - 0.01, // Shift slightly south
+  };
+
   return (
     <div style={{ height: '100%', width: '100%' }}>
       <Map
         mapboxAccessToken={mapboxToken}
-        initialViewState={{
-          longitude: -122.4,
-          latitude: 37.8,
-          zoom: 12,
-        }}
+        initialViewState={initialView}
         style={{ width: '100%', height: '100%' }}
         mapStyle="mapbox://styles/mapbox/dark-v10"
       >
         <Marker
-          longitude={-122.4}
-          latitude={37.8}
+          longitude={initialMarker.longitude}
+          latitude={initialMarker.latitude}
         >
           <div
             style={{
